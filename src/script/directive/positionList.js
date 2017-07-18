03d0829e4ab9ee573c395c0523b3088c861ebe98
fengxiaoqing128 <daisyfeng128@foxmail.com>
2017-07-18 08:13:57 +0800

    data-bind and directive

--- src/script/directive/positionList.js
+++ src/script/directive/positionList.js
@@ -3,7 +3,10 @@ angular.module('app').directive('appPositionList',[function(){
 	return{
 		restrict: 'A',
 	    replace: true,
-	    templateUrl: 'view/template/positionList.html'
+	    templateUrl: 'view/template/positionList.html',
+	    $scope:{
+	    	data:'='  //降低控制器与指令的耦合度，提高代码复用率
+	    }
 	}
 }])
 
