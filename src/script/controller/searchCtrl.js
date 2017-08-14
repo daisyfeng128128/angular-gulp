cde0306aa97d7c72a74ee93673baf3e28c28f67d
fengxiaoqing128 <daisyfeng128@foxmail.com>
2017-08-14 23:09:38 +0800

    search

--- src/script/controller/searchCtrl.js
+++ src/script/controller/searchCtrl.js
@@ -1,5 +1,13 @@
 'use strict';
 angular.module('app').controller('searchCtrl', ['$http', '$scope', function($http, $scope){
+	$scope.name = '';
+	$scope.search = function(){
+		$http.get('data/positionList.json?name='+$scope.name).success(function(resp){
+			$scope.positionList = resp;
+		});
+	}
 	
+	$scope.search();
+
 
 }])
